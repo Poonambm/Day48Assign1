2518846025607d6e03abf2a81a2df27a6ef4bee8
Poonambm <poonammanusmare@gmail.com>
2022-05-04 13:27:10 -0400

    first commit

---
+++ src/Casting.java
@@ -0,0 +1,16 @@
+class Casting {
+    public static void main(String[] args)  {
+       // Step 1
+        double data = 112.35;
+       
+        System.out.println("Double: " + data);
+        int num = (int)data;
+        System.out.println("int: "+ num);
+        //Step 2
+        String data1 = "49";
+          System.out.println("The string value is: " + data1);
+          int num1 = Integer.parseInt(data1);
+          System.out.println("The integer value is: " + num1);
+    }
+}
+
